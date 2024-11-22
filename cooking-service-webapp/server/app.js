25290999e735b0261e3c9fc5ae1f5827d4a2c68a
arcanlte <pinangayjoel@gmail.com>
2024-11-22 00:01:58 -0500

    removed comment

--- cooking-service-webapp/server/app.js
+++ cooking-service-webapp/server/app.js
@@ -4,10 +4,8 @@ const cors = require("cors");
 const foodRoutes = require("./routes/foodRoutes");
 
 app.use(cors());
-//middleware
 app.use(express.json());
 
-//this is called "mounting routes"
 app.use("/api", foodRoutes);
 
 module.exports = app;
